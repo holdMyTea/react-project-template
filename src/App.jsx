002ba8bcaa269af81c3428cd3c1c051e13d9c5cf
holdMyTea <oleksandr.pohrebniak42@gmail.com>
2020-02-13 08:52:54 -0500

    Include ESLint

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () => (
-  <h2>'Sup, bro!</h2>
+  <h2>&apos;Sup, bro!</h2>
 )
 
 export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -27,4 +27,4 @@ module.exports = {
       inject: 'body'
     })
   ]
-};
+}
